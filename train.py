0f9a39b5d50a7a73857f36d11a60cbd63876b7ab
dungtd2403 <dung.td2403@gmail.com>
2022-12-26 17:14:05 +0700

    Update train.py to train kitti with BP

--- train.py
+++ train.py
@@ -1,17 +1,18 @@
 import tensorflow as tf
-from model.model import DepthAwareNet, ParameterizedNet, BackboneSharedParameterizedNet, Kitti3DPredictor
+from model.model import DepthAwareNet, ParameterizedNet, BackboneSharedParameterizedNet, Kitti3DPredictor, Kitti3DBP
 from model.loss import L2DepthLoss, L2NormRMSE
 from solver.optimizer import OptimizerFactory
 import argparse
+# import torch
 
 tf.keras.backend.clear_session()
 
 parser = argparse.ArgumentParser(description='Select between small or big data',
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument('-d', '--data-size', type=str, choices=['big', 'small', 'real', 'kitti'], default='small')
-parser.add_argument('-m', '--training-mode', type=str, choices=['normal', 'parameterized', 'shared', 'kitti'], default='parameterized')
-parser.add_argument('-b', '--batch-size', type=int, default=32)
+parser.add_argument('-d', '--data-size', type=str, choices=['big', 'small', 'real', 'kitti', 'kittiBP'], default='kittiBP')
+parser.add_argument('-m', '--training-mode', type=str, choices=['normal', 'parameterized', 'shared', 'kitti'], default='shared')
+parser.add_argument('-b', '--batch-size', type=int, default=16)
 parser.add_argument('-j', '--jobs', type=int,  default=8)
 
 args = parser.parse_args()
@@ -27,24 +28,40 @@ if args.data_size == 'big':
     val_path = "./val_new.csv"
     img_directory = "/media/data/teamAI/phuc/phuc/airsim/data"
 elif args.data_size == 'kitti':
-    train_path = "/media/data/teamAI/minh/kitti_out/kitti_train.csv"
-    val_path = "/media/data/teamAI/minh/kitti_out/kitti_val.csv"
-    img_directory = "/media/data/teamAI/minh/kitti_out/semantic-0.4"
+    train_path = "/home/dung/ivsr-s2p/kitti_airsim2412/2022-12-23-18-30-33/train.csv"
+    val_path = "/home/dung/ivsr-s2p/kitti_airsim2412/2022-12-23-18-30-33/test.csv"
+    img_directory = "/home/dung/ivsr-s2p/kitti_airsim2412/2022-12-23-18-30-33/images"
+    
+elif args.data_size == 'kittiBP':
+    train_path = "/home/dung/ivsr-s2p/kitti_airsim2412/2022-12-23-18-30-33/train.csv"
+    val_path = "/home/dung/ivsr-s2p/kitti_airsim2412/2022-12-23-18-30-33/test.csv"
+    img_directory = "/home/dung/ivsr-s2p/kitti_airsim2412/2022-12-23-18-30-33/images"
 else:
     train_path = "./train588_50_new.csv"
     val_path = "./val588_50_new.csv"
     img_directory = "/media/data/teamAI/phuc/phuc/airsim/50imperpose/full/"
+
+
+
 
 if args.training_mode =='normal':
     from data.parallel_dataset import Dataset, DataLoader
 else:
     from data.parameterized_parallel_dataset import Dataset, DataLoader
     
+# dataset = Dataset(train_path, img_directory, input_shape)
 train_dataset = Dataset(train_path, img_directory, input_shape)
 val_dataset = Dataset(val_path, img_directory, input_shape)
 
+# split_ratio = 0.8
+# train_size = int(split_ratio * len(dataset))
+# val_size = len(dataset) - train_size
+# train_set, val_set = torch.utils.data.random_split(dataset, [train_size, val_size])
+
 train_loader = DataLoader(train_dataset, input_shape=input_shape, batch_size=args.batch_size, num_parallel_calls=args.jobs)
 val_loader = DataLoader(val_dataset, input_shape=input_shape, batch_size=args.batch_size, num_parallel_calls=args.jobs)
+
+
 
 ################
 # Define model #
@@ -55,6 +72,8 @@ elif args.training_mode == 'shared':
     net = BackboneSharedParameterizedNet(num_ext_conv=1)
 elif args.data_size == 'kitti':
     net = Kitti3DPredictor(num_ext_conv=1)
+elif args.data_size == 'kittiBP':
+    net = Kitti3DBP(num_ext_conv=1)
 else:
     net = DepthAwareNet(num_ext_conv=0)
 
@@ -89,7 +108,7 @@ else:
 trainer = Trainer(train_loader, val_loader=val_loader,
                     model=net, distance_loss_fn=dist_loss_fn, depth_loss_fn=depth_loss_fn,
                     optimizer=optimizer,
-                    log_path='/media/data/teamAI/minh/ivsr-logs/training_kitti1507.txt', savepath='/media/data/teamAI/minh/ivsr_weights/training_kitti1507',
+                    log_path='/home/dung/ivsr-s2p/log/training_kitti24012.txt', savepath='/home/dung/ivsr-s2p/ivsr_weights/training_kitti1507',
                     use_mse=USE_MSE)
 
 _  = trainer.train(30, save_checkpoint=True, early_stop=True)
